d087ebe8a17ea7d6ab5f51ac60fba83b802991a3
Swastik <swastikpanda32@gmail.com>
2024-02-03 21:36:13 +0530

    added userInput state

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,27 +1,64 @@
 import React from "react";
+import { useState } from "react";
 
 const UserInput = () => {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 10,
+    duration: 6,
+  });
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  };
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={userInput.initialInvestment}
+            onChange={(e) => handleChange("initialInvestment", e.target.value)}
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={userInput.annualInvestment}
+            onChange={(e) => handleChange("annualInvestment", e.target.value)}
+          />
         </p>
       </div>
 
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={userInput.expectedReturn}
+            onChange={(e) => handleChange("expectedReturn", e.target.value)}
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={userInput.duration}
+            onChange={(e) => handleChange("duration", e.target.value)}
+          />
         </p>
       </div>
     </section>
